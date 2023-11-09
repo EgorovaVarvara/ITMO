0563da482fc2a4c62f8d61c2ff9f90cbb1d86b48
EgorovaVarvara <147907865+EgorovaVarvara@users.noreply.github.com>
2023-11-09 22:21:16 +0300

    Add files via upload

---
+++ programming/lab2/src/ru/itmo/lab2/moves/Cradily/Bulldoze.java
@@ -0,0 +1,21 @@
+package ru.itmo.lab2.moves.Cradily;
+
+import ru.ifmo.se.pokemon.*;
+
+public class Bulldoze extends PhysicalMove {
+    public Bulldoze(double pow, double acc) {
+        super(Type.GROUND, pow, acc);
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p) {
+        super.applyOppEffects(p);
+        Effect EffectObject = new Effect().stat(Stat.SPEED, -1);
+        p.addEffect(EffectObject);
+    }
+
+    @Override
+    protected String describe() {
+        return "бьет по всему вокруг, утрамбовывая землю";
+    }
+}
