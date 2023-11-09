8bd131dda76ad37dc6a29daf6b2085ac9deea930
EgorovaVarvara <147907865+EgorovaVarvara@users.noreply.github.com>
2023-11-09 23:42:33 +0300

    Add files via upload

---
+++ programming/lab2/src/ru/itmo/lab2/moves/Nidorino/Flatter.java
@@ -0,0 +1,22 @@
+package ru.itmo.lab2.moves.Nidorino;
+
+import ru.ifmo.se.pokemon.*;
+
+public class Flatter extends SpecialMove {
+    public Flatter(double pow, double acc) {
+        super(Type.DARK, pow, acc);
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p) {
+        super.applyOppEffects(p);
+        Effect EffectObject = new Effect().stat(Stat.SPECIAL_ATTACK, 1);
+        p.addEffect(EffectObject);
+        Effect.confuse(p);
+    }
+
+    @Override
+    protected String describe() {
+        return "говорит лестные слова";
+    }
+}
